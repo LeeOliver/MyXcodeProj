b58963ff53642c9b57bc8813b1bea4c9cdc6d978
LeeOliver <15852049096@sina.cn>
2013-09-02 15:28:33 +0800

    9.2CreatProject
    New a picture project

---
+++ PreviewPicture/PreviewPicture/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PreviewPicture
+//
+//  Created by apple on 13-9-2.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
